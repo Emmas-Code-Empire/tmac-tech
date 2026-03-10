88d832caba58f371ca803d6549476cb15d64b7d1
Michael Kanahele <mike@suppscale.io>
2026-03-10 18:40:26 -0500

    Prisma

--- apps/tmac-apps/src/app/login/page.tsx
+++ apps/tmac-apps/src/app/login/page.tsx
@@ -4,8 +4,19 @@ import styles from "./page.module.css";
 import { useEffect, useState } from "react";
 import { Session } from "next-auth";
 import { signIn, signOut } from "next-auth/react";
+import { auth_GetSession } from "@/actions/authActions";
 
 export default function Home() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    const getAndSetSession = async () => {
+      const session = await auth_GetSession();
+      setSession(session);
+    };
+    getAndSetSession();
+  }, []);
+
   return (
     <div className={styles.pageContainer}>
       <form
@@ -15,6 +26,17 @@ export default function Home() {
       >
         <button type="submit">Signin with Google</button>
       </form>
+      {session?.user && (
+        <form
+          action={async () => {
+            await signOut();
+          }}
+        >
+          <button type="submit">Sign Out</button>
+        </form>
+      )}
+
+      <p>{session?.user ? "Logged In" : "Signed Out"}</p>
     </div>
   );
 }
