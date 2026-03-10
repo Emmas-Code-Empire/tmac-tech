3c599b25f7ca80d530ca93bb8d7dc86c557b3d78
Michael Kanahele <mike@suppscale.io>
2026-03-10 18:08:01 -0500

    Prisma added

---
+++ packages/prisma/src/index.ts
@@ -0,0 +1,3 @@
+import prisma from "./prisma";
+export * from "./generated/prisma";
+export default prisma;
---
+++ packages/prisma/src/prisma.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const prismaClientSingleton = () => {
+  const adapter = new PrismaNeon({
+    connectionString: process.env.DATABASE_URL!,
+  });
+  return new PrismaClient({ adapter });
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
